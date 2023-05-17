3969ca26cf13629002ce92c9fd87e378b8c37e94
Zdravko Bonev <zdravko.bonev@era.io>
2023-05-17 13:44:03 +0300

    Removed FoundWords component and use only function because is not require extensive state management or lifecycle hooks. Using a function that returns JSX can be sufficient. It keeps the code concise and straightforward.

--- src/components/FoundWords.js
+++ src/components/FoundWords.js
@@ -3,101 +3,95 @@ import { Grid, Divider, List, ListItem, ListItemText } from "@mui/material";
 import { words } from "../components/Words";
 import "../App.css";
 
-const FoundWords = ({ rows, columns, tableData }) => {
-  const dfs = (table, visited, row, col, word, currentWord, found) => {
-    if (word === "") {
-      const length = currentWord.length;
-      if (!found.hasOwnProperty(`words${length}`)) {
-        found[`words${length}`] = []; // Create a new list for words of this length
-      }
-      if (!found[`words${length}`].includes(currentWord)) {
-        found[`words${length}`].push(currentWord);
-      }
-      return;
+const dfs = (table, visited, row, col, word, currentWord, found) => {
+  if (word === "") {
+    const length = currentWord.length;
+    if (!found.hasOwnProperty(`words${length}`)) {
+      found[`words${length}`] = []; // Create a new list for words of this length
     }
-
-    if (row >= 0 && row < table.length && col >= 0 && col < table[row].length) {
-      if (!visited[row][col] && table[row][col] === word[0]) {
-        visited[row][col] = true;
-
-        const newWord = currentWord + table[row][col];
-
-        dfs(table, visited, row - 1, col, word.slice(1), newWord, found); // Up
-        dfs(table, visited, row + 1, col, word.slice(1), newWord, found); // Down
-        dfs(table, visited, row, col - 1, word.slice(1), newWord, found); // Left
-        dfs(table, visited, row, col + 1, word.slice(1), newWord, found); // Right
-
-        visited[row][col] = false;
-      }
+    if (!found[`words${length}`].includes(currentWord)) {
+      found[`words${length}`].push(currentWord);
     }
-  };
+    return;
+  }
 
-  const iterateTable = (rows, cols, tableData, words) => {
-    const foundWords = {};
-    const startTime = performance.now();
+  if (row >= 0 && row < table.length && col >= 0 && col < table[row].length) {
+    if (!visited[row][col] && table[row][col] === word[0]) {
+      visited[row][col] = true;
 
-    for (let i = 0; i < words.length; i++) {
-      const visited = new Array(rows - 0)
-        .fill(false)
-        .map(() => new Array(cols).fill(false));
-      const word = words[i];
+      const newWord = currentWord + table[row][col];
 
-      for (let j = 0; j < rows; j++) {
-        for (let k = 0; k < cols; k++) {
-          dfs(tableData, visited, j, k, word, "", foundWords);
-        }
-      }
+      dfs(table, visited, row - 1, col, word.slice(1), newWord, found); // Up
+      dfs(table, visited, row + 1, col, word.slice(1), newWord, found); // Down
+      dfs(table, visited, row, col - 1, word.slice(1), newWord, found); // Left
+      dfs(table, visited, row, col + 1, word.slice(1), newWord, found); // Right
+
+      visited[row][col] = false;
     }
-    const endTime = performance.now();
-    const timeTaken = endTime - startTime;
-    const count = Object.values(foundWords).reduce(
-      (accumulator, currentValue) => accumulator + currentValue.length,
-      0
-    );
-
-    return (
-      <Grid container justifyContent="center" marginTop={10}>
-        <Grid
-          container
-          justifyContent="center"
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-        >
-          <h2>{`Time taken: ${timeTaken.toFixed(2)} milliseconds`}</h2>
-          <h2>{`Found ${count} words`}</h2>
-        </Grid>
-
-        <List className="list-component">
-          {Object.entries(foundWords).map(([key, value]) => (
-            <React.Fragment key={key}>
-              {value.length !== 0 && (
-                <>
-                  <ListItem disablePadding>
-                    <Grid
-                      item
-                      container
-                      xs={12}
-                      display="flex"
-                      flexDirection="column"
-                    >
-                      <h4>{`Words with ${key.slice(5)} letters:`}</h4>
-                      <ListItemText primary={value.join(", ")} />
-                    </Grid>
-                  </ListItem>
-                  <Grid>
-                    <Divider variant="fullWidth" />
-                  </Grid>
-                </>
-              )}
-            </React.Fragment>
-          ))}
-        </List>
-      </Grid>
-    );
-  };
-
-  return iterateTable(rows, columns, tableData, words);
+  }
 };
 
-export default FoundWords;
+export const iterateTable = (rows, cols, tableData) => {
+  const foundWords = {};
+  const startTime = performance.now();
+
+  for (let i = 0; i < words.length; i++) {
+    const visited = new Array(rows - 0)
+      .fill(false)
+      .map(() => new Array(cols).fill(false));
+    const word = words[i];
+
+    for (let j = 0; j < rows; j++) {
+      for (let k = 0; k < cols; k++) {
+        dfs(tableData, visited, j, k, word, "", foundWords);
+      }
+    }
+  }
+  const endTime = performance.now();
+  const timeTaken = endTime - startTime;
+  const count = Object.values(foundWords).reduce(
+    (accumulator, currentValue) => accumulator + currentValue.length,
+    0
+  );
+
+  return (
+    <Grid container justifyContent="center" marginTop={10}>
+      <Grid
+        container
+        justifyContent="center"
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+      >
+        <h2>{`Time taken: ${timeTaken.toFixed(2)} milliseconds`}</h2>
+        <h2>{`Found ${count} words`}</h2>
+      </Grid>
+
+      <List className="list-component">
+        {Object.entries(foundWords).map(([key, value]) => (
+          <React.Fragment key={key}>
+            {value.length !== 0 && (
+              <>
+                <ListItem disablePadding>
+                  <Grid
+                    item
+                    container
+                    xs={12}
+                    display="flex"
+                    flexDirection="column"
+                  >
+                    <h4>{`Words with ${key.slice(5)} letters:`}</h4>
+                    <ListItemText primary={value.join(", ")} />
+                  </Grid>
+                </ListItem>
+                <Grid>
+                  <Divider variant="fullWidth" />
+                </Grid>
+              </>
+            )}
+          </React.Fragment>
+        ))}
+      </List>
+    </Grid>
+  );
+};
